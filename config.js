88b502a479a5e98c88401221d2031805c2566ea0
Pmethnuwani <pmethnuwani@gmail.com>
2025-12-17 21:30:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?dbVFkM'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?FYaSLQ'
 };
